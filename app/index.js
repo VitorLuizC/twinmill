f98126132b73e9f5cf36c076c1cae2b97d1045c4
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-01-05 20:23:12 -0200

    :lipstick:

--- app/index.js
+++ app/index.js
@@ -1,24 +1,21 @@
 "use strict";
 
 const express = require("express");
-const config = require("./config");
+const { server } = require("./config");
 const router = require("./router");
 const bodyParser = require("body-parser");
 
 /**
  * Express instance.
  */
-let application = express();
+express()
+  .use(cors)
+  .use(bodyParser.json())
+  .use("/", router)
+  .listen(server.port, server.port, server.callback);
 
 function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }
-
-application.use(cors);
-application.use(bodyParser.json());
-
-application.use("/", router);
-
-application.listen(config.server.port, config.server.port, config.server.callback);
--- app/router/index.js
+++ app/router/index.js
@@ -6,12 +6,10 @@ const author = require("./author.js");
 /**
  * Express's Router instance.
  */
-let router = express.Router();
+const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({});
-});
-
-router.use("/author", author);
+router
+  .get("/", (req, res) => res.json({}))
+  .use("/author", author);
 
 module.exports = router;
